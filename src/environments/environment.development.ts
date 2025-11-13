e31413367caf49149681f564148f90987f7c3432
Dennis <tdennis.developer@gmail.com>
2025-11-13 14:08:31 +0100

    Delete src/environments directory

--- src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: 'https://code-pay.onrender.com'
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: true,
-    apiUrl: 'https://code-pay.onrender.com'
-};
